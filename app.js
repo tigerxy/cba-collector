fd742274ff09e76db99b248246f9eadd6afe6481
Roland Greim <roland@stempelgreim.de>
2018-12-14 23:46:15 +0100

    basic location functions

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var tokenRouter = require('./routes/token');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/map', tokenRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
