e141ff947e354f920593a044a4d7efef55784f25
Roland Greim <roland@stempelgreim.de>
2018-12-14 23:54:54 +0100

    index fixed

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CBA' });
 });
 
+router.post('/', function(req, res, next) {
+  var token = req.body.token;
+  if (token != undefined)
+    res.redirect('/map/'+req.body.token);
+  else
+    res.redirect('/');
+});
+
 module.exports = router;
