471d734f8776aea5f8280dbec49a44a6505d8172
Roland Greim <roland@stempelgreim.de>
2018-12-25 19:13:51 +0100

    pretty changes

--- routes/token.js
+++ routes/token.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/:token', function(req, res, next) {
+router.get('/:token', function (req, res, next) {
   res.render('map', { title: "CBA", token: req.params.token });
 });
 
