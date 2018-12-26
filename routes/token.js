2d8535aa295814fa953e08afae3881ef2b97e0bd
Roland Greim <roland@stempelgreim.de>
2018-12-26 04:08:31 +0100

    read head from file

--- routes/token.js
+++ routes/token.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/:token', function (req, res, next) {
-  var revision = require('child_process')
-    .execSync('git rev-parse --short HEAD')
-    .toString().trim();
+  var revision = fs.readFileSync('.git/HEAD').toString();
+  if (revision.indexOf(':') != -1) {
+    revision = fs.readFileSync('.git/' + revision.substring(5).trim()).toString();
+  }
   res.render('map', { title: "CBA", token: req.params.token, git: revision });
 });
 
