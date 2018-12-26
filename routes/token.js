5603958e108b2bd069ca0616d023a33f091ad2b3
Roland Greim <roland@stempelgreim.de>
2018-12-26 04:09:58 +0100

    azure can not read head

--- routes/token.js
+++ routes/token.js
@@ -1,13 +1,11 @@
 var express = require('express');
-var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/:token', function (req, res, next) {
-  var revision = fs.readFileSync('.git/HEAD').toString();
-  if (revision.indexOf(':') != -1) {
-    revision = fs.readFileSync('.git/' + revision.substring(5).trim()).toString();
-  }
+  var revision = require('child_process')
+    .execSync('git rev-parse --short HEAD')
+    .toString().trim();
   res.render('map', { title: "CBA", token: req.params.token, git: revision });
 });
 
