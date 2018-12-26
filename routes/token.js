6b53c1cfbb13a703a149d92af0f92e514eb9c133
Roland Greim <roland@stempelgreim.de>
2018-12-26 06:07:50 +0100

    x

--- routes/token.js
+++ routes/token.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/:token', function (req, res, next) {
-  var revision = require('child_process')
+  /*var revision = require('child_process')
     .execSync('git rev-parse --short HEAD')
-    .toString().trim();
+    .toString().trim();*/
   res.render('map', { title: "CBA", token: req.params.token, git: revision });
 });
 
