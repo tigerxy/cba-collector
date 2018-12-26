3839a2b469c987060a021a311d348ed63a76d4a9
Roland Greim <roland@stempelgreim.de>
2018-12-26 06:09:37 +0100

    git head removed

--- routes/token.js
+++ routes/token.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/:token', function (req, res, next) {
-  /*var revision = require('child_process')
-    .execSync('git rev-parse --short HEAD')
-    .toString().trim();*/
-  res.render('map', { title: "CBA", token: req.params.token, git: revision });
+  res.render('map', { title: "CBA", token: req.params.token });
 });
 
 module.exports = router;
